c3b853b26507c255f2c38d4b34941d698e75c617
blackguo <blackguo@tencent.com>
2020-11-24 12:06:37 +0800

    init

---
+++ src/main/java/com/blackguo/patterns/FactoryPattern/Shape.java
@@ -0,0 +1,5 @@
+package com.blackguo.patterns.FactoryPattern;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/main/java/com/blackguo/patterns/StrategyPattern/Strategy.java
@@ -0,0 +1,5 @@
+package com.blackguo.patterns.StrategyPattern;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
